87c52d8dd33e77d013da255ab1c0edb1bcbda4a2
Gino Bejarano <ginobejarano@gmail.com>
2017-10-19 07:13:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var greeting = "Hello, everybody";
-console.log(greeting)
+
 
 var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Let's talk about space";
+var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `Let's talk about ${topic}.`;
+
